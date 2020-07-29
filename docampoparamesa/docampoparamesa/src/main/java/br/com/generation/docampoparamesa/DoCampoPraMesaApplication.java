a1cbf21424f5bec5b8f4590f430f7de58819737f
vivianreis <srtvivian@hotmail.com>
2020-07-29 18:55:40 -0300

    atividade 2 criação com spring initializr

---
+++ docampoparamesa/docampoparamesa/src/main/java/br/com/generation/docampoparamesa/DoCampoPraMesaApplication.java
@@ -0,0 +1,13 @@
+package br.com.generation.docampoparamesa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoCampoPraMesaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DoCampoPraMesaApplication.class, args);
+	}
+
+}
