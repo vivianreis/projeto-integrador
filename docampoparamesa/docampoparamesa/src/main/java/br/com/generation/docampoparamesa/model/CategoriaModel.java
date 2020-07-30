30a06d263434e4ae895820d70a19e4b1e0c9f67b
vivianreis <srtvivian@hotmail.com>
2020-07-30 20:38:05 -0300

    Criação CategoriaModel

---
+++ docampoparamesa/docampoparamesa/src/main/java/br/com/generation/docampoparamesa/model/CategoriaModel.java
@@ -0,0 +1,50 @@
+package br.com.generation.docampoparamesa.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Table;
+import javax.validation.constraints.Size;
+
+import com.sun.istack.NotNull;
+
+@Entity
+@Table(name = "categoria")
+public class CategoriaModel {
+	
+	@javax.persistence.Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long Id;
+    
+	@NotNull
+	@Size(min = 5, max = 255)
+	private String nome;
+	
+	@NotNull
+	@Size(min = 5, max = 255)
+	private String setor;
+	
+	public long getId() {
+		return Id;
+	}
+
+	public void setId(long id) {
+		Id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getSetor() {
+		return setor;
+	}
+
+	public void setSetor(String setor) {
+		this.setor = setor;
+	}
+}
