66e7001ed52aaacda36138148a36e3ac49e767ee
vivianreis <srtvivian@hotmail.com>
2020-08-27 20:15:19 -0300

    texto de apresentação da empresa + imagens de consumidores

--- docampoparamesa/docampoparamesa/doCampoPraMesa/src/app/app-routing.module.ts
+++ docampoparamesa/docampoparamesa/doCampoPraMesa/src/app/app-routing.module.ts
@@ -1,8 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { QuemSomosComponent } from './quem-somos/quem-somos.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full'},
+  { path: 'home', component: HomeComponent },
+  { path: 'quem-somos', component: QuemSomosComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
