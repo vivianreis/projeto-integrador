6c13bac63e236ca2aa1166b749957a9d8e5779fc
Paula <paula_figueiredo@ymail.com>
2020-08-27 14:44:51 -0300

    branch

--- docampoparamesa/docampoparamesa/doCampoPraMesa/src/app/app-routing.module.ts
+++ docampoparamesa/docampoparamesa/doCampoPraMesa/src/app/app-routing.module.ts
@@ -2,7 +2,9 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
+
 const routes: Routes = [];
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
