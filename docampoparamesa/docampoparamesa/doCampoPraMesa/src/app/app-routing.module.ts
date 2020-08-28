01edc428070bc3adfcf607703cbcd33044701576
unknown <moniquejroliveira@gmail.com>
2020-08-27 22:17:51 -0300

    Atualização de rotas e da aba contato; Em suporte com a Paula

--- docampoparamesa/docampoparamesa/doCampoPraMesa/src/app/app-routing.module.ts
+++ docampoparamesa/docampoparamesa/doCampoPraMesa/src/app/app-routing.module.ts
@@ -1,8 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { QuemSomosComponent } from './quem-somos/quem-somos.component';
+import { ContatoComponent } from './contato/contato.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', redirectTo: 'home', pathMatch: 'full'},
+  {path: 'home', component: HomeComponent},
+  {path: 'quem-somos', component: QuemSomosComponent},
+  {path: 'contato', component: ContatoComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
